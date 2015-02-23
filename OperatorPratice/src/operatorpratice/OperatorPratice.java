f23e38fe55bf1a3b6e707e9e47d765b7f78f049c
class <class@CST_N417_22.cst.citytech.cuny.edu>
2015-02-23 17:09:34 -0500

    operator practice

---
+++ OperatorPratice/src/operatorpratice/OperatorPratice.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package operatorpratice;
+
+/**
+ *
+ * @author class
+ */
+public class OperatorPratice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    double testScore1 = 90;
+    double testScore2 = 90;
+    double testScore3 = 89;
+    System.out.println("The score for test 1 is " + testScore1 + ".");
+    System.out.println("The score for test 2 is " + testScore2 + ".");
+    System.out.println("The score for test 3 is " + testScore3 + ".");
+    double average;
+    average = (testScore1 + testScore2 + testScore3)/3;
+    System.out.println("The average test score is " + average + "." );
+    }
+    
+}
